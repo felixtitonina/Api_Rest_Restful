c37b3444f0a16b2216ac0d5ea542692f84ccfccb
felixtitonina <felixtitonina@outlook.com>
2020-01-02 01:36:58 -0300

    Cadastro de user com senha hash, jwt

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const authConfig = require('../config/auth')
 
 const User = require('../models/user') // pode ser min*
 
@@ -18,5 +22,23 @@ router.post('/register', async (req, res) => {
         return res.status(400).send({ error: 'Registration failed' });
     }
 });
+
+router.post('/authenticate', async (req, res) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email }).select('+password')
+
+    if (!user)
+        return res.status(400).send({ error: 'User not found' })
+
+    if (!await bcrypt.compare(password, user.password))
+        return res.status(400).send({ error: 'Invalid password' })
+
+    user.password = undefined;
+
+    const token = jwt.sign({ id: user.id }, authConfig.secret,{
+        expiresIn: 86400
+    });
+    res.send({ user, token })
+})
 
 module.exports = app => app.use('/auth', router)
